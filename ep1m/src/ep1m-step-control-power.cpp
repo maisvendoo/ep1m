93b35d4bd4ca472ad3cb990d0aa9ccbec82fe40d
maisvendoo <maisvendoo@gmail.com>
2021-02-07 15:55:38 +0300

    Связаны основные модули АКБ и ШП-21

--- ep1m/src/ep1m-step-control-power.cpp
+++ ep1m/src/ep1m-step-control-power.cpp
@@ -5,7 +5,14 @@
 //------------------------------------------------------------------------------
 void EP1m::stepControlPower(double t, double dt)
 {
+    battery->setChargeVoltage(power_supply->getChargeVoltage());
+    battery->setLoadCurrent(0.0);
     battery->step(t, dt);
 
-    Ucc = max(0.0, battery->getVoltage());
+    power_supply->setBatVoltage(battery->getVoltage());
+    power_supply->setBatChargeCurrent(battery->getCargeCurrent());
+    power_supply->setInputVoltage(0.0);
+    power_supply->step(t, dt);
+
+    Ucc = power_supply->getPowerControlVoltage();
 }
