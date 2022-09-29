08c2d7b766fc4231f4ae3b6defab8c823aa97fff
maisvendoo <maisvendoo@yandex.ru>
2022-09-29 19:27:42 +0300

    Зафиксированы крайние изменения

--- ep1m/include/ep1m-signals.h
+++ ep1m/include/ep1m-signals.h
@@ -10,18 +10,18 @@ enum
 
     // Тумблеры блока выключателей
     SIGNAL_TUMBLER_MSUD = 1, // МСУД
-    SIGNAL_TUMBLER_LOCK_VVK = 2, // Блокирование ВВК
-    SIGNAL_TUMBLER_PANT1 = 3, // Токоприемник 1
-    SIGNAL_TUMBLER_PANT2 = 4, // Токоприемник 2
-    SIGNAL_TUMBLER_RETURN_PROTECTION = 5, // Возврат защиты ГВ
-    SIGNAL_TUMBLER_MAIN_SWITCH = 6, // Главный выключатель
+    SIGNAL_TUMBLER_LOCK_VVK = 31, // Блокирование ВВК
+    SIGNAL_TUMBLER_PANT1 = 32, // Токоприемник 1
+    SIGNAL_TUMBLER_PANT2 = 33, // Токоприемник 2
+    SIGNAL_TUMBLER_RETURN_PROTECTION = 35, // Возврат защиты ГВ
+    SIGNAL_TUMBLER_MAIN_SWITCH = 36, // Главный выключатель
 
-    SIGNAL_TUMBLER_AUX_MACHINES = 7, // Вспомогательные машины
-    SIGNAL_TUMBLER_COMPRESSOR = 8, // Компрессор
-    SIGNAL_TUMBLER_MOTOR_FAN1 = 9, // Вентилятор 1
-    SIGNAL_TUMBLER_MOTOR_FAN2 = 10, // Вентилятор 2
-    SIGNAL_TUMBLER_MOTOR_FAN3 = 11, // Вентилятор 3
-    SIGNAL_TUMBLER_EPT = 12, // ЭПТ
+    SIGNAL_TUMBLER_AUX_MACHINES = 65, // Вспомогательные машины
+    SIGNAL_TUMBLER_COMPRESSOR = 66, // Компрессор
+    SIGNAL_TUMBLER_MOTOR_FAN1 = 67, // Вентилятор 1
+    SIGNAL_TUMBLER_MOTOR_FAN2 = 68, // Вентилятор 2
+    SIGNAL_TUMBLER_MOTOR_FAN3 = 69, // Вентилятор 3
+    SIGNAL_TUMBLER_EPT = 100, // ЭПТ
 
     // Колесные пары
     SIGNAL_WHEEL1 = 194, // Колесная пара 1
