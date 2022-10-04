91ef12951b3485f6620d2714339d42511c71b2b7
maisvendoo <maisvendoo@gmail.com>
2022-10-04 14:33:37 +0300

    Болванка для ПЧФ

--- ep1m-equipment/include/freq-phase-converter.h
+++ ep1m-equipment/include/freq-phase-converter.h
@@ -1,8 +1,31 @@
 #ifndef     FREQ_PHASE_CONVERTER_H
 #define     FREQ_PHASE_CONVERTER_H
 
+#include    "device.h"
+
 //------------------------------------------------------------------------------
 //  Преобразователь частоты и числа фаз (ПЧФ)
 //------------------------------------------------------------------------------
+class FreqPhaseConverter : public Device
+{
+public:
+
+    FreqPhaseConverter(QObject *parent = Q_NULLPTR);
+
+    ~FreqPhaseConverter();
+
+    double getFrequency() const { return freq_cur; }
+
+private:
+
+    /// Текущая частота питания вспомогательных машин
+    double freq_cur;
+
+    void preStep(state_vector_t &Y, double t);
+
+    void ode_system(const state_vector_t &Y, state_vector_t &dYdt, double t);
+
+    void load_config(CfgReader &cfg);
+};
 
 #endif // FREQ_PHASE_CONVERTER_H
