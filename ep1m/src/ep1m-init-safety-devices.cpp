845e0f1b03aced182895a2573390e209afaa6375
maisvendoo <maisvendoo@yandex.ru>
2023-06-20 00:00:56 +0300

    прикручиваем ЭК к КУЛБ-У

--- ep1m/src/ep1m-init-safety-devices.cpp
+++ ep1m/src/ep1m-init-safety-devices.cpp
@@ -1,5 +1,7 @@
 #include    "ep1m.h"
 
+#include    <QDir>
+
 //------------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------------
@@ -7,4 +9,23 @@ void EP1m::initSafetyDevices()
 {
     klub_BEL = new KLUB();
     klub_BEL->setMaxVelocity(140.0);
+
+    QString path = QDir::toNativeSeparators(route_dir) + QDir::separator() +
+            "stations.conf";
+
+    klub_BEL->loadStationsMap(path);
+
+    QString speeds_name = "speeds";
+
+    if (dir > 0)
+        speeds_name += "1";
+    else
+        speeds_name += "2";
+
+    path = QDir::toNativeSeparators(route_dir) +
+            QDir::separator() + speeds_name + ".conf";
+
+    klub_BEL->loadSpeedsMap(path);
+    klub_BEL->setDirection(dir);
+    klub_BEL->setTrainLength(50.0);
 }
