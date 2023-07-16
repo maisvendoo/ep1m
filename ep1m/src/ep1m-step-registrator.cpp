08f9e6241d1d25aef64f206c5d89a9cf29c2765d
maisvendoo <maisvendoo@yandex.ru>
2023-07-16 18:15:16 +0300

    Добавил документацию и пополнил список регистрируемых параметров

--- ep1m/src/ep1m-step-registrator.cpp
+++ ep1m/src/ep1m-step-registrator.cpp
@@ -20,11 +20,16 @@ double EP1m::calcTracForce()
 //------------------------------------------------------------------------------
 void EP1m::stepRegistration(double t, double dt)
 {
-    QString line = QString("%1 %2 %3 %4")
-            .arg(t, 10, 'f', 1)
-            .arg(velocity * Physics::kmh, 6, '2', 1)
-            .arg(trac_motor[0]->getAncorCurrent(), 7, 'f', 2)
-            .arg(calcTracForce() / 1000.0, 6, '2', 1);
+    QString line = QString("%1 %2 %3 %4 %5 %6 %7 %8 %9")
+            .arg(t, 10, 'f', 1) // Текущее время симуляции
+            .arg(velocity * Physics::kmh, 6, '2', 1) // Текущая скорость
+            .arg(trac_motor[0]->getAncorCurrent(), 7, 'f', 2) // Ток якоря
+            .arg(trac_motor[0]->getFieldCurrent(), 7, 'f', 2) // Ток возбуждения
+            .arg(calcTracForce() / 1000.0, 6, '2', 1) // Сила тяги/ЭДТ
+            .arg(main_reservoir->getPressure(), 7, 'f', 3) // Давление ГР
+            .arg(brakepipe->getPressure(), 7, 'f', 3) // Давление ТМ
+            .arg(supply_reservoir->getPressure(), 7, 'f', 3) // Давление ЗР
+            .arg(brake_mech[TROLLEY_FWD]->getBCpressure(), 7, 'f', 3); // Давление ТЦ
 
     registrator->print(line, t, dt);
 }
