c3142ed713f819a9167940a31cef192b69d92717
maisvendoo <maisvendoo@yandex.ru>
2024-07-19 22:31:24 +0300

    Наконец-то разобрался с корректной переконвертацией звуков

--- ep1m/src/ep1m-step-sounds.cpp
+++ ep1m/src/ep1m-step-sounds.cpp
@@ -8,6 +8,6 @@ void EP1m::stepSoundSignals(double t, double dt)
     (void) t;
     (void) dt;
 
-    analogSignal[SOUND_SVISTOK] = horn->getSvistokSound().createSoundSignal();
-    analogSignal[SOUND_TIFON] = horn->getTifonSound().createSoundSignal();
+    analogSignal[SOUND_SVISTOK] = horn->getSoundSignal(TrainHorn::SVISTOK_SOUND);
+    analogSignal[SOUND_TIFON] = horn->getSoundSignal(TrainHorn::TIFON_SOUND);
 }
