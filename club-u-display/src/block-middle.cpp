0efe347d6d2042537bccf76f4fe1451ec27d5706
Mikhail Arepev <arepmm@gmail.com>
2025-06-23 13:07:07 +0300

    Вывод скорости, конфиг к анимации дисплея

--- club-u-display/src/block-middle.cpp
+++ club-u-display/src/block-middle.cpp
@@ -13,7 +13,7 @@ MiddleBlock::MiddleBlock(QSize _size, QString cfg_path, QWidget *parent)
     , txtCurSpeedLimit_(Q_NULLPTR)
 {
     this->resize(_size);
-    this->setStyleSheet("border: 1px solid red");
+    //this->setStyleSheet("border: 1px solid red");
 
     // Спидометр
     speedometer_ = new Speedometer(_size, cfg_path, this);
@@ -23,17 +23,17 @@ MiddleBlock::MiddleBlock(QSize _size, QString cfg_path, QWidget *parent)
     reverseInd_->move(214, 363);
 
     // Числовая индикация скорости
-    txtCurSpeed_ = new TextPaint(QSize(100,50), this);
-    txtCurSpeed_->setFonts(33, Qt::green);
-    txtCurSpeed_->move(135, 84);
-    txtCurSpeed_->setParams(3, 33);
+    txtCurSpeed_ = new TextPaint(QSize(132,59), this);
+    txtCurSpeed_->setFonts(39, Qt::green);
+    txtCurSpeed_->move(177, 126);
+    txtCurSpeed_->setParams(3, 44);
     txtCurSpeed_->setText(QString("000"));
 
     // Числовая индикация ограничения скорости
-    txtCurSpeedLimit_ = new TextPaint(QSize(100,50), this);
-    txtCurSpeedLimit_->setFonts(33, Qt::red);
-    txtCurSpeedLimit_->move(135, 159);
-    txtCurSpeedLimit_->setParams(3, 33);
+    txtCurSpeedLimit_ = new TextPaint(QSize(132,59), this);
+    txtCurSpeedLimit_->setFonts(39, Qt::red);
+    txtCurSpeedLimit_->move(177, 226);
+    txtCurSpeedLimit_->setParams(3, 44);
     txtCurSpeedLimit_->setText(QString("000"));
 
 
