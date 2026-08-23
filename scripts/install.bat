cb0c151035730c05d8eee0e6a25c026336ec4372
Mikhail Arepev <arepmm@gmail.com>
2026-08-23 15:04:46 +0300

    Корректное копирование 3d-модели с ktx-текстурами в скрипте установки в dev-версию

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,5 +1,7 @@
 echo "Hi!"
 
 xcopy /E /Y ..\cfg\*.* %RRS_DEV_ROOT%\cfg\
-xcopy /E /Y ..\data\*.* %RRS_DEV_ROOT%\data\
+xcopy /E /Y ..\data\animations\*.* %RRS_DEV_ROOT%\data\animations\
+xcopy /E /Y ..\data\models_ktx\*.* %RRS_DEV_ROOT%\data\models\
+xcopy /E /Y ..\data\sounds\*.* %RRS_DEV_ROOT%\data\sounds\
 xcopy /E /Y ..\..\modules\*.* %RRS_DEV_ROOT%\modules
